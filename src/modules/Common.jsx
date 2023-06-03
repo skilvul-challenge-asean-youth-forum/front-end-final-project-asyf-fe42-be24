9b9d1a0834bbc5e2b43e13dd023a0cdd8adaaae0
dika841 <randikaa1999@gmail.com>
2023-06-03 23:17:40 +0700

    improve : navbar background blur on scroll

---
+++ src/modules/Common.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+export const useScrollPosition = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    let timeoutId;
+
+    const handleScroll = () => {
+      setPosition({ x: window.scrollX, y: window.scrollY });
+      setIsScrolling(true);
+
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        setIsScrolling(false);
+      }, 500);
+    };
+
+    setPosition({ x: window.scrollX, y: window.scrollY });
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return { position, isScrolling };
+};
