37967de0b36b29e4c076095f09254a25176f2b14
dika841 <randikaa1999@gmail.com>
2023-06-08 21:48:49 +0700

    Create-feature : Forum page & Improve :Input search to component ,create Title Container component ,Change icons on ButtonLink

--- src/layouts/content-layout/index.jsx
+++ src/layouts/content-layout/index.jsx
@@ -1,19 +1,7 @@
 import PropTypes from "prop-types";
-import { BsArrowRightShort } from "react-icons/bs";
-const ContentLayout = ({ children, title, label }) => {
+const ContentLayout = ({ children }) => {
   return (
-    <section className="w-full min-h-screen px-16 py-8">
-      <div className="flex justify-between w-full mb-10">
-        <h1 className="text-3xl font-bold underline underline-offset-8 decoration-[#237804] decoration-3">
-          {title}
-        </h1>
-        <button className="text-sky-500 flex items-center text-xl">
-          {label}
-          <BsArrowRightShort size={25} />
-        </button>
-      </div>
-      {children}
-    </section>
+    <section className="w-full min-h-screen px-16 py-8">{children}</section>
   );
 };
 
