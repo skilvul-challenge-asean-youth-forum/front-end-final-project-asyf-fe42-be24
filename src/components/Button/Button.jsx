767b87186dde42e14282ac6f8f9e42fd370e0994
Ishadireza <mishadirezap@gmail.com>
2023-06-16 21:31:07 +0700

    Create : Notification modal component          Integrate to Detailforum page

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,9 @@
-const Button = ({ name, bgColor, textColor, type }) => {
+const Button = ({ name, bgColor, textColor, type, onClick }) => {
   return (
     <button
       type={type}
       className={`inline-flex justify-center items-center py-2 px-5 text-base font-medium text-center ${textColor} rounded-lg ${bgColor} hover:bg-lime-500 hover:text-white focus:ring-2 ring-slate-50`}
+      onClick={onClick}
     >
       {name}
     </button>
